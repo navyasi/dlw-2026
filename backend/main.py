7b86698068aeff42d5506edddad3c32402b0a851
Yajie Wang <ywang238@e.ntu.edu.sg>
2026-03-06 15:53:02 +0800

    mapping to port 8080

--- backend/main.py
+++ backend/main.py
@@ -67,7 +67,7 @@ app = FastAPI(title="Study Mode API", version="1.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:5500", "http://127.0.0.1:5500"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:8080", "http://127.0.0.1:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
